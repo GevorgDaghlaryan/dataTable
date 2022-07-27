8744e0f83ceed45f437e461767e6c666cf90b1d8
Gevorg Daghlaryan <92582092+GevorgDaghlaryan@users.noreply.github.com>
2022-07-27 22:44:52 +0400

    lesson 2

--- assets/js/DataTable.js
+++ assets/js/DataTable.js
@@ -2,7 +2,7 @@ class DataTable {
   constructor(columns = [], data = []) {
     this.columns = columns;
     this.data = data;
-    this.newData=this.newData
+    this.notesPage = 2;
   }
 
   createTable() {
@@ -10,24 +10,20 @@ class DataTable {
     this.table = $table;
     const $dataTableContainer = document.querySelector('.data-table-container');
     $dataTableContainer.appendChild($table);
-
-   
     this.createThead($table);
     const $tbody = this.createTbody();
     $table.appendChild($tbody);
-    const $trs = this.renderData(this.data.slice(0,2));
-    
+    const $trs = this.renderData(this.data.slice(0,this.notesPage));
     $trs.forEach(($tr) => {
       $tbody.appendChild($tr);
     });
-
-    this.pagination(this.data);
+    this.pagination(this.data,this.notesPage);
   }
 
   createThead(parent) {
     const $thead = document.createElement('thead');
     const $tr = document.createElement('tr');
-    this.columns.forEach((column) => {
+    this.columns.forEach((column)=> {
       const $th = document.createElement('th');
       $th.innerHTML = column;
       $tr.appendChild($th);
@@ -35,7 +31,6 @@ class DataTable {
 
     $thead.appendChild($tr);
     parent.appendChild($thead);
-  
   }
 
   createTbody() {
@@ -46,63 +41,85 @@ class DataTable {
   renderData(dat  = []) {
     return dat.map((item) => {
       const $tr = document.createElement('tr');
+
       for(const key in item) {
         const $td = document.createElement('td');
         $td.innerHTML = item[key];
         $tr.appendChild($td);
       }
-      console.log($tr)
+
       return $tr;
     });
   }
 
  
- pagination(info) {
-  let self = this;
-  const notesPage = 2;
-  const countItems = Math.ceil(info.length/notesPage);
-  const $ul = document.getElementById('pagination');
+  pagination(info, itemCount) {
+  let self = this; 
+  const countItems = Math.ceil(info.length / itemCount);
+  const $ul = document.createElement('ul');
+  $ul.setAttribute('id', 'pagination');
+  const $dataTableContainer = document.querySelector('.data-table-container');
+  $dataTableContainer.appendChild($ul)
   let items = [];
-  let active  ; 
+  let active; 
 
   for(let i = 1; i <= countItems; i++){
     let $li = document.createElement('li');
     $li.innerHTML = i;
+
     if(i === 1 ) {
       $li.className = 'active';
       active = $li;
     }
+
     $ul.appendChild($li);
     items.push($li);   
   
   };
 
   for (let item of items){
-    item.addEventListener('click',function(){
-      console.log({active});
+    item.addEventListener('click', function(){
+
       if(active){
-        console.log(887);
         active.classList.remove('active')
       }
+
       active = this;
       this.classList.add('active');
       let pageNum = this.innerHTML;
-      let start = (pageNum - 1) * notesPage;
-      let end = start + notesPage;
+      let start = (pageNum - 1) *  itemCount;
+      let end = start + itemCount;
       let newData =  info.slice(start,end);
-
       let newPageItem = self.renderData(newData);
       let newPageContainer = document.querySelector('tbody');
       newPageContainer.innerHTML = null;
-
-      newPageContainer.append(...newPageItem);
-     
-      
+      newPageContainer.append(...newPageItem); 
     })
   }
+  this.createPerPage();
+  } 
 
-  
-}
+  createPerPage() {
+  const $dataTableContainer = document.querySelector('.data-table-container');
+  const $select = document.createElement('select');
+  const $table = document.querySelector('.data-table-container>table');
+  const $ul = document.querySelector('.data-table-container>ul');;
+
+  for(let i = 2; i < 7; i = i+2){
+    let $option = document.createElement('option');
+    $option.innerHTML = i;
+    $select.appendChild($option);
+  }
+
+  $dataTableContainer.appendChild($select);
+  $select.addEventListener('change', (e)=>{
+    this.notesPage = + e.target.value;
+    $dataTableContainer.removeChild($table); 
+    $dataTableContainer.removeChild($ul); 
+    $dataTableContainer.removeChild($select); 
+    this.createTable(this.data);
+   })
+  }
 }
 
 export default DataTable;
--- assets/js/index.js
+++ assets/js/index.js
@@ -9,19 +9,20 @@ const data = [
   {id: 4, name: 'Ani', age: 22,},
   {id: 5, name: 'Anna', age: 18,},
   {id: 6,name: 'Aram',age: 23,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
-  {id: 7,name: 'Arsen',age: 19,},
+  {id: 8,name: 'Arsen',age: 19,},
+  {id: 9,name: 'Aren',age: 18,},
+  {id: 10,name: 'Artur',age: 19,},
+  {id: 11,name: 'Astxik',age: 15,},
+  {id: 12,name: 'Arsen',age: 19,},
+  {id: 13,name: 'Arsen',age: 17,},
+  {id: 14,name: 'Arsen',age: 19,},
+  {id: 15 ,name: 'Arsen',age: 20,},
 ];
 
 const dataTable = new DataTable(columns, data);
 
 dataTable.createTable();
+//dataTable.createPagination();
 
 
 // Element.prototype.dataTable = function() {
